5fd872afb498cc28f2823bb3b57bc17479eca6c7
Taranjeet Singh <105316020+Taran234@users.noreply.github.com>
2023-04-11 21:49:47 -0400

    Add files via upload

--- book_map.py
+++ book_map.py
@@ -4,19 +4,8 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from collections import defaultdict
 
-import os
-import fitz
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from collections import defaultdict
 
-import os
-import fitz
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from collections import defaultdict
-
-def summarize_and_map(file_path, bullet_points=True, font_size=16):
+def summarize_and_map(file_path, map_size=5, min_chunk_len=3, max_chunk_len=5):
     # Extract text from PDF file
     with fitz.open(file_path) as doc:
         text = ''
@@ -29,40 +18,39 @@ def summarize_and_map(file_path, bullet_points=True, font_size=16):
     sentences = [sent for sent in doc.sents if len(sent) > 10]
     phrase_scores = defaultdict(int)
     for phrase in doc.noun_chunks:
-        if len(phrase) > 1 and phrase.text.lower() not in STOP_WORDS:
+        if min_chunk_len <= len(phrase) <= max_chunk_len and phrase.text.lower() not in STOP_WORDS:
             phrase_scores[phrase.text] += phrase.root.dep_ in {'nsubj', 'dobj', 'pobj'}
     sentence_scores = defaultdict(int)
     for sent in sentences:
         for phrase in sent.noun_chunks:
             if phrase.text in phrase_scores:
                 sentence_scores[sent] += phrase_scores[phrase.text]
-    summary = sorted(sentence_scores, key=sentence_scores.get, reverse=True)[:5]
+    summary = sorted(sentence_scores, key=sentence_scores.get, reverse=True)[:map_size]
 
-    # Create mind map as HTML file
+    # Create mind map as HTML file with tree layout
     title = os.path.splitext(os.path.basename(file_path))[0]
-    mind_map = f'<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>{title}</title>\n<style>\nbody {{ font-family: Arial, sans-serif; font-size: {font_size}px; }}\nul {{ list-style: {"disc" if bullet_points else "none"}; padding-left: {"30px" if bullet_points else "0px"}; }}\n.topic {{ font-size: {font_size*1.5}px; font-weight: bold; color: #414a4c; }}\n.sent {{ margin-left: {"30px" if bullet_points else "0px"}; }}\n</style>\n</head>\n<body>\n<h1><center>{title}</center></h1>\n'
+    with open('index.html', 'r') as f:
+        html_code = f.read()
+
+    mind_map = f'{html_code}\n'
     topics = defaultdict(list)
     for sentence in summary:
-        topic = sentence.root.text.capitalize()
-        if topic not in topics:
-            topics[topic].append(sentence)
-        else:
-            topics[topic].append(sentence)
+        for phrase in sentence.noun_chunks:
+            if min_chunk_len <= len(phrase) <= max_chunk_len:
+                topic = phrase.text.capitalize()
+                topics[topic].append(sentence)
     for topic, sentences in topics.items():
-        mind_map += f'<h2 class="topic">{topic}</h2>\n<ul>\n'
+        mind_map += f'<li><span class="topic">{topic}</span>\n<ul>\n'
         for sent in sentences:
             mind_map += f'<li class="sent">{sent}</li>\n'
-        mind_map += '</ul>\n'
+        mind_map += '</ul></li>\n'
 
-    mind_map += '</body>\n</html>\n'
+    mind_map += '</ul>\n</body>\n</html>\n'
 
     # Save mind map as HTML file
     with open(f'{title}.html', 'w', encoding='utf-8') as f:
         f.write(mind_map)
     print(f'Mind map saved to {os.getcwd()}\\{title}.html')
-
-
-
 
 if __name__ == '__main__':
     file_path = input('Enter file path: ')
